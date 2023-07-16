6a6fc113ca9be72d3135219515fc749159a1da41
Aris Bimas Nugroho <arisbimas2@gmail.com>
2023-07-16 22:28:58 +0700

    top bar mobile

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -16,14 +16,14 @@ export const TopBar: React.FC<TopBarProps> = ({ handleMenuClick }) => {
 
   return (
     <header className="bg-brand-cream-pastel mx-auto h-24 inset-x-0 top-0 z-50 fixed flex items-center justify-center">
-      <nav className="lg:w-9/12 w-10/12 flex justify-between m-auto">
+      <nav className="lg:w-9/12 w-11/12 flex justify-between m-auto">
         <div
           className="logo"
           onClick={() => handleMenuClick(menuRef?.homeRef?.current)}
         >
           <Image src={Logo} alt="logo" unoptimized={true} />
         </div>
-        <div className="flex gap-x-2 md:gap-x-6 items-center text-zinc-700">
+        <div className="flex gap-x-1 md:gap-x-6 items-center text-zinc-700">
           <a
             // key={item.name}
             // href={item.href}
