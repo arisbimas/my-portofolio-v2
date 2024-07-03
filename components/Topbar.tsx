76a40da76f6a96d7b29b81e47c08c9029d84df8e
Aris Bimas Nugroho <arisbimas2@gmail.com>
2024-07-03 12:21:39 +0700

    feat: add download cv and favicon

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import MenuContext from "@/context/MenuContext";
 
 const anchorStyle =
-  "px-3 py-1 border border-brand-cream-pastel rounded-md text-md md:text-lg font-semibold md:font-medium cursor-pointer hover:bg-brand-pink-pastel hover:border-black hover:shadow-[1.8px_2px_0px_0_rgba(0,0,0)] hover:ease-out hover:duration-500 hover:translate-y-1";
+  "px-1 md:px-3 py-1 border border-brand-cream-pastel rounded-md text-sm md:text-lg font-semibold md:font-medium cursor-pointer hover:bg-brand-pink-pastel hover:border-black hover:shadow-[1.8px_2px_0px_0_rgba(0,0,0)] hover:ease-out hover:duration-500 hover:translate-y-1";
 
 interface TopBarProps {
   handleMenuClick: (index: number) => void;
@@ -14,9 +14,13 @@ interface TopBarProps {
 export const TopBar: React.FC<TopBarProps> = ({ handleMenuClick }) => {
   const menuRef = useContext(MenuContext);
 
+  const handleDownloadResume = () => {
+    window.open("https://drive.google.com/file/d/14_-z4EE-kFDlkHBfTy-rje0r7mhipUnk/view?usp=drive_link", "_blank");
+  }
+
   return (
-    <header className="bg-brand-cream-pastel mx-auto h-24 inset-x-0 top-0 z-50 fixed flex items-center justify-center">
-      <nav className="lg:w-9/12 w-10/12 flex justify-between m-auto">
+    <header className="bg-brand-cream-pastel mx-auto h-24 w-full top-0 z-50 fixed flex items-center justify-center">
+      <nav className="lg:w-9/12 w-12/12 flex gap-4 justify-between mx-auto">
         <div
           className="logo"
           onClick={() => handleMenuClick(menuRef?.homeRef?.current)}
@@ -47,6 +51,14 @@ export const TopBar: React.FC<TopBarProps> = ({ handleMenuClick }) => {
             onClick={() => handleMenuClick(menuRef?.contactRef?.current)}
           >
             Contact
+          </a>
+          <a
+            // key={item.name}
+            // href={item.href}
+            className={"flex gap-1 items-center px-3 py-1 border rounded-md text-sm md:text-lg font-semibold md:font-medium cursor-pointer bg-[#F8CB46] border-black shadow-[1.8px_2px_0px_0_rgba(0,0,0)] hover:ease-out hover:duration-500 hover:translate-y-1"}
+            onClick={handleDownloadResume}
+          >
+            CV Resume
           </a>
         </div>
       </nav>
