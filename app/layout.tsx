cb7c4909f47e504e8980d374b39a38f23e7fac47
Aris Bimas Nugroho <arisbimas2@gmail.com>
2025-09-24 13:19:07 +0700

    feat: add vercel analytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 
 import LayoutContent from "@/components/LayoutContent";
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <LayoutContent>{children}</LayoutContent>
+        <Analytics />
       </body>
     </html>
   );
